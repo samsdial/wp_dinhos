c6b58aa1adb7e2233a20c165c2d7ce6759a445ab
samsdial <diastudios@gmail.com>
2018-05-22 17:29:50 -0500

    Add Ajustes En page National league

--- wp-content/themes/theme-cgr/single-national.php
+++ wp-content/themes/theme-cgr/single-national.php
@@ -3,9 +3,6 @@
     <div class="box_intro--img" style="background-image: url(<?php echo get_field('imagen')[url]; ?>);height: 300px;background-size: cover;background-repeat:  no-repeat;background-position: top center;"></div>
 </main>
 
-
-
-
 <main class="middle pd_top--x50 pd_bottom--x100">
     <section class="breadcrumb container pd_bottom--x10">
         <ul class="list_breadcrumb list--inline txt_upper">
@@ -16,7 +13,7 @@
         <div class="bdr_solid bdr_color--blue bdr_width--x1 mg_top--x10 mg_bottom--x20"></div>
     </section>
     <section class="intro mg_bottom--x50">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col">
                     <div class="title txt_upper cl--blue mg_bottom--x10">
@@ -31,7 +28,7 @@
                 </div>
                 <!-- Ultimo Partido -->
                 <div class="col">
-                    <?php echo get_field('proximo_paritido'); ?>
+                    <?php echo get_field('proximo_partido'); ?>
                 </div>
             </div>
             <div class="row">
@@ -39,18 +36,100 @@
                     <div class="breadcrumb container pd_bottom--x10">
                         <h4>Tabla</h4>
                     </div>
-                    <div style="display: block; margin-bottom: 15px">
+                    <div class="pd_bottom--x10">
                         <?php echo get_field('tabla'); ?>
                     </div>
                 </div>
+                <!--Acordion Fixture-->
                 <div class="col">
-                    <?php echo get_field('fixture'); ?>
+                    <div class="breadcrumb container pd_bottom--x10">
+                        <h4>Fixtures / Calendario</h4>
+                    </div>
+                    <div class="accordion" id="accordionExample">
+                        <?php if( get_field('fixture') ): ?>
+                            <?php while ( has_sub_field('fixture') ) :?>
+                                <?php if( get_row_layout() == 'acordeon_date4' ): ?>
+                                    <div class="card">
+                                        <div class="card-header" id="headingOne">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                                    <span class="talleres aicon" ></span>
+                                                    <span>
+                                            <?php the_sub_field('title'); ?>
+                                        </span>
+                                                </button>
+                                            </h5>
+                                        </div>
+                                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample" style="">
+                                            <div class="card-body ">
+                                                <?php the_sub_field('date'); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if( get_row_layout() == 'acordeon_date3' ): ?>
+                                    <div class="card">
+                                        <div class="card-header" id="headingTwo">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                    <span class="talleres aicon"></span>
+                                                    <span><?php the_sub_field('title'); ?></span>
+                                                </button>
+                                            </h5>
+                                        </div>
+                                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample" style="">
+                                            <div class="card-body cl--blue">
+                                                <?php the_sub_field('date'); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if( get_row_layout() == 'acordeon_date2' ): ?>
+                                    <div class="card">
+                                        <div class="card-header" id="headingThree">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                                                    <span class="talleres aicon" ></span>
+                                                    <span>
+                                            <?php the_sub_field('title'); ?>
+                                        </span>
+                                                </button>
+                                            </h5>
+                                        </div>
+                                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample" style="">
+                                            <div class="card-body cl--blue">
+                                                <?php the_sub_field('date'); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                                <?php if( get_row_layout() == 'acordeon_date1' ): ?>
+                                    <div class="card">
+                                        <div class="card-header" id="headingfour">
+                                            <h5 class="mb-0">
+                                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                                                    <span class="talleres aicon"></span>
+                                                    <span><?php the_sub_field('title');?></span>
+                                                </button>
+                                            </h5>
+                                        </div>
+                                        <div id="collapsefour" class="collapse" aria-labelledby="headingfour" data-parent="#accordionExample" style="">
+                                            <div class="card-body cl--blue">
+                                                <?php the_sub_field('date'); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endif; ?>
+                            <?php endwhile; ?>
+                        <?php else : ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <?php echo get_field('fixture'); ?>
-                </div>
+                <!--<div class="col">
+                    <?php /*echo get_field('fixture'); */?>
+                </div>-->
                 <div class="col">
                     <?php echo get_field('calendario'); ?>
                 </div>
