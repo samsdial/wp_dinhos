0677e6a8ba7b32a8acf89dd7d4a676be51e68625
samsdial <diastudios@gmail.com>
2018-06-12 17:14:19 -0500

    Add FE Acordion fixture new

--- wp-content/themes/theme-cgr/single-national.php
+++ wp-content/themes/theme-cgr/single-national.php
@@ -62,17 +62,17 @@
                     <div class="bgc--red cl--white p-4">
                         <h4>Fixtures / Calendario <small>(<?php the_title(); ?>)</small></h4>
                     </div>
-                    <div class="accordion" id="accordionExample">
-                        <?php if( get_field('fixture') ): ?>
-                            <?php while ( has_sub_field('fixture') ) :?>
-                                <?php if( get_row_layout() == 'acordeon_date4' ): ?>
+                    <!--<div class="accordion" id="accordionExample">
+                        <?php /*if( get_field('fixture') ): */?>
+                            <?php /*while ( has_sub_field('fixture') ) :*/?>
+                                <?php /*if( get_row_layout() == 'acordeon_date4' ): */?>
                                     <div class="card">
                                         <div class="card-header" id="headingOne">
                                             <button class="btn btn_ghost" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                                             </button>
                                             <h5 class="title mb-0 p-3 cl--red txt_size--em2">
                                                 <span class="icon icon-angle-down mr-2"></span>
-                                                <span><?php the_sub_field('title'); ?></span>
+                                                <span><?php /*the_sub_field('title'); */?></span>
                                                 <span class="circle card bgc--silver cl--white">
                                                     <span style="font-size: 21px">vs</span>
                                                 </span>
@@ -80,19 +80,19 @@
                                         </div>
                                         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample" style="">
                                             <div class="card-body ">
-                                                <?php the_sub_field('date'); ?>
+                                                <?php /*the_sub_field('date'); */?>
                                             </div>
                                         </div>
                                     </div>
-                                <?php endif; ?>
-                                <?php if( get_row_layout() == 'acordeon_date3' ): ?>
+                                <?php /*endif; */?>
+                                <?php /*if( get_row_layout() == 'acordeon_date3' ): */?>
                                     <div class="card">
                                         <div class="card-header" id="headingTwo">
                                             <button class="btn btn_ghost" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseOne">
                                             </button>
                                             <h5 class="title mb-0 p-3 cl--red txt_size--em2">
                                                 <span class="icon icon-angle-down mr-2"></span>
-                                                <span><?php the_sub_field('title'); ?></span>
+                                                <span><?php /*the_sub_field('title'); */?></span>
                                                 <span class="circle card bgc--silver cl--white">
                                                     <span style="font-size: 21px">vs</span>
                                                 </span>
@@ -101,19 +101,19 @@
                                         </div>
                                         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample" style="">
                                             <div class="card-body cl--blue">
-                                                <?php the_sub_field('date'); ?>
+                                                <?php /*the_sub_field('date'); */?>
                                             </div>
                                         </div>
                                     </div>
-                                <?php endif; ?>
-                                <?php if( get_row_layout() == 'acordeon_date2' ): ?>
+                                <?php /*endif; */?>
+                                <?php /*if( get_row_layout() == 'acordeon_date2' ): */?>
                                     <div class="card">
                                         <div class="card-header" id="headingThree">
                                             <button class="btn btn_ghost" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseOne">
                                             </button>
                                             <h5 class="title mb-0 p-3 cl--red txt_size--em2">
                                                 <span class="icon icon-angle-down mr-2"></span>
-                                                <span><?php the_sub_field('title'); ?></span>
+                                                <span><?php /*the_sub_field('title'); */?></span>
                                                 <span class="circle card bgc--silver cl--white">
                                                     <span style="font-size: 21px">vs</span>
                                                 </span>
@@ -121,19 +121,19 @@
                                         </div>
                                         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample" style="">
                                             <div class="card-body cl--blue">
-                                                <?php the_sub_field('date'); ?>
+                                                <?php /*the_sub_field('date'); */?>
                                             </div>
                                         </div>
                                     </div>
-                                <?php endif; ?>
-                                <?php if( get_row_layout() == 'acordeon_date1' ): ?>
+                                <?php /*endif; */?>
+                                <?php /*if( get_row_layout() == 'acordeon_date1' ): */?>
                                     <div class="card">
                                         <div class="card-header" id="headingfour">
                                             <button class="btn btn_ghost" type="button" data-toggle="collapse" data-target="#collapsefour" aria-expanded="true" aria-controls="collapseOne">
                                             </button>
                                             <h5 class="title mb-0 p-3 cl--red txt_size--em2">
                                                 <span class="icon icon-angle-down mr-2"></span>
-                                                <span><?php the_sub_field('title'); ?></span>
+                                                <span><?php /*the_sub_field('title'); */?></span>
                                                 <span class="circle card bgc--silver cl--white">
                                                     <span style="font-size: 21px">vs</span>
                                                 </span>
@@ -141,14 +141,46 @@
                                         </div>
                                         <div id="collapsefour" class="collapse show" aria-labelledby="headingfour" data-parent="#accordionExample" style="">
                                             <div class="card-body cl--blue">
-                                                <?php the_sub_field('date'); ?>
+                                                <?php /*the_sub_field('date'); */?>
                                             </div>
                                         </div>
                                     </div>
-                                <?php endif; ?>
-                            <?php endwhile; ?>
+                                <?php /*endif; */?>
+                            <?php /*endwhile; */?>
+                        <?php /*else : */?>
+                        <?php /*endif; */?>
+                    </div>-->
+                    <div class="accordion" id="accteam">
+                        <?php
+                        if( have_rows('new_fixture') ):
+                            while ( have_rows('new_fixture') ) : the_row(); ?>
+                                <div class="card">
+                                    <div class="card-header" id="headinga<?php the_sub_field('new_id'); ?> ">
+                                        <button class="btn btn_ghost" type="button" data-toggle="collapse" data-target="#<?php the_sub_field('new_id'); ?>" aria-expanded="false" aria-controls="collapseOne">
+                                        </button>
+                                        <h5 class="title mb-0 p-3 cl--red txt_size--em2">
+                                            <span class="icon icon-angle-down mr-2"></span>
+                                            <span><?php the_sub_field('new_title'); ?></span>
+                                            <span class="circle card bgc--silver cl--white">
+                                                    <span style="font-size: 21px">vs</span>
+                                                </span>
+                                        </h5>
+                                    </div>
+                                    <div id="<?php the_sub_field('new_id'); ?>" class="collapse" aria-labelledby="heading<?php the_sub_field('new_id'); ?>" data-parent="#accteam" style="">
+                                        <div class="card-body ">
+                                            <?php the_sub_field('new_date'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            <?php   endwhile; ?>
+
                         <?php else : ?>
+                            <div class="text-center">
+                                <p> No info</p>
+                            </div>
                         <?php endif; ?>
+
                     </div>
                 </div>
             </div>
@@ -171,3 +203,5 @@
         <div class="bdr_solid bdr_color--blue bdr_width--x1 mg_top--x10 mg_bottom--x20"></div>
     </section>
 </main>
+
+
